f3e5ea31d6b6827476a75b6b2d242995cde44f42
王晓峰 <xiaofeng.wang@wizarpos.com>
2019-05-12 20:37:30 +0800

    手动按键rfid

--- app/src/main/java/me/factory/ScanActivity.java
+++ app/src/main/java/me/factory/ScanActivity.java
@@ -5,16 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-
+import android.view.View;
+import android.view.View.OnClickListener;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ScanActivity extends AppCompatActivity {
+public class ScanActivity extends AppCompatActivity implements OnClickListener {
 
     private DeviceManager deviceManager;
-
+    private Handler handler;
     private Set<String> epsc = new HashSet<>();
 
     @Override
@@ -38,20 +40,47 @@ public class ScanActivity extends AppCompatActivity {
             }
         });
         deviceManager.init();
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //rfid工具类初始化后，进行设置rfid功率，此功率参数需要在vue页面做持久化保存，并在每次打开wms时，传递到以下方法做设置
-                // ，应为程序关闭后，功率设置就会无效，所以需要每次打开程序都传递下功率参数，以调节rfid距离
-                //启动和设置rfid, 需要有个1秒的时间间隔，否则会失败
-                int range = getIntent().getIntExtra("rfidrange", 30);//取不到值情况下，默认30
-                deviceManager.rfidRangeSet(range);
-                deviceManager.start();
-            }
-        }, 1000L);
+        //Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                //rfid工具类初始化后，进行设置rfid功率，此功率参数需要在vue页面做持久化保存，并在每次打开wms时，传递到以下方法做设置
+//                // ，应为程序关闭后，功率设置就会无效，所以需要每次打开程序都传递下功率参数，以调节rfid距离
+//                //启动和设置rfid, 需要有个1秒的时间间隔，否则会失败
+//                int range = getIntent().getIntExtra("rfidrange", 30);//取不到值情况下，默认30
+//                deviceManager.rfidRangeSet(range);
+//                deviceManager.start();
+//            }
+//        }, 1000L);
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==131){
+            handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //rfid工具类初始化后，进行设置rfid功率，此功率参数需要在vue页面做持久化保存，并在每次打开wms时，传递到以下方法做设置
+                    // ，应为程序关闭后，功率设置就会无效，所以需要每次打开程序都传递下功率参数，以调节rfid距离
+                    //启动和设置rfid, 需要有个1秒的时间间隔，否则会失败
+                    int range = getIntent().getIntExtra("rfidrange", 30);//取不到值情况下，默认30
+                    deviceManager.rfidRangeSet(range);
+                    deviceManager.start();
+                }
+            }, 100L);
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onResume() {
--- app/src/main/java/me/factory/ScanMoreActivity.java
+++ app/src/main/java/me/factory/ScanMoreActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -25,6 +26,7 @@ public class ScanMoreActivity extends AppCompatActivity {
     private Set<String> epsc = new HashSet<>();
     private ArrayAdapter<String> adapter;
     private List<String> firm = new ArrayList<String>();
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,18 +46,18 @@ public class ScanMoreActivity extends AppCompatActivity {
             }
         });
         deviceManager.init();
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //rfid工具类初始化后，进行设置rfid功率，此功率参数需要在vue页面做持久化保存，并在每次打开wms时，传递到以下方法做设置
-                // ，应为程序关闭后，功率设置就会无效，所以需要每次打开程序都传递下功率参数，以调节rfid距离
-                //启动和设置rfid, 需要有个1秒的时间间隔，否则会失败
-                int range = getIntent().getIntExtra("rfidrange", 30);//取不到值情况下，默认30
-                deviceManager.rfidRangeSet(range);
-                deviceManager.start();
-            }
-        }, 1000L);
+//        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                //rfid工具类初始化后，进行设置rfid功率，此功率参数需要在vue页面做持久化保存，并在每次打开wms时，传递到以下方法做设置
+//                // ，应为程序关闭后，功率设置就会无效，所以需要每次打开程序都传递下功率参数，以调节rfid距离
+//                //启动和设置rfid, 需要有个1秒的时间间隔，否则会失败
+//                int range = getIntent().getIntExtra("rfidrange", 30);//取不到值情况下，默认30
+//                deviceManager.rfidRangeSet(range);
+//                deviceManager.start();
+//            }
+//        }, 1000L);
         EpcList = (ListView) findViewById(R.id.listView_epclist);
         adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, firm);
@@ -92,6 +94,25 @@ public class ScanMoreActivity extends AppCompatActivity {
             return "RFID编号:" + epc + "\n" ;
         }
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==131){
+            handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //rfid工具类初始化后，进行设置rfid功率，此功率参数需要在vue页面做持久化保存，并在每次打开wms时，传递到以下方法做设置
+                    // ，应为程序关闭后，功率设置就会无效，所以需要每次打开程序都传递下功率参数，以调节rfid距离
+                    //启动和设置rfid, 需要有个1秒的时间间隔，否则会失败
+                    int range = getIntent().getIntExtra("rfidrange", 30);//取不到值情况下，默认30
+                    deviceManager.rfidRangeSet(range);
+                    deviceManager.start();
+                }
+            }, 100L);
+        }
+        return false;
     }
 
     @Override
