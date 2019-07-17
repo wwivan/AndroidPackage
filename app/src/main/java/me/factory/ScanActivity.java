8a35469f944b218a87b057d8dbb2cf45d6b1cb13
王晓峰 <xiaofeng.wang@wizarpos.com>
2019-07-17 14:40:10 +0800

    修改红外和rfid同时

--- app/src/main/java/me/factory/ScanActivity.java
+++ app/src/main/java/me/factory/ScanActivity.java
@@ -2,27 +2,44 @@ package me.factory;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemProperties;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+
 import java.util.HashSet;
 import java.util.Set;
 
-public class ScanActivity extends AppCompatActivity implements OnClickListener {
+public class ScanActivity extends AppCompatActivity implements OnClickListener,MyReceiver.Message {
 
     private DeviceManager deviceManager;
     private Handler handler;
     private Set<String> epsc = new HashSet<>();
+    private String RECE_DATA_ACTION = "com.se4500.onDecodeComplete";
+    private String START_SCAN_ACTION = "com.geomobile.se4500barcode";
+    private String STOP_SCAN = "com.geomobile.se4500barcode.poweroff";
+    private MyReceiver receiver;
+    private TextView tv_scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(RECE_DATA_ACTION);
+        filter.addAction(START_SCAN_ACTION);
+        filter.addAction(STOP_SCAN);
+        tv_scan = (TextView) findViewById(R.id.tv_scan);
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+        receiver.setMessage(this);
         deviceManager = new DeviceManagerImpl(this,0);
         deviceManager.setListener(new DeviceManager.Listener() {
             @Override
@@ -54,6 +71,21 @@ public class ScanActivity extends AppCompatActivity implements OnClickListener {
 //        }, 1000L);
     }
 
+    private void startScan() {
+        SystemProperties.set("persist.sys.scanstopimme", "false");
+        Intent intent = new Intent();
+        intent.setAction(START_SCAN_ACTION);
+        sendBroadcast(intent, null);
+    }
+    @Override
+    public void getMsg(String str) {
+        //通过实现MyReceiver.Message接口可以在这里对MyReceiver中的数据进行处理
+        tv_scan.setText(str);
+        Intent result = new Intent();
+        result.putExtra("result", str);
+        setResult(801, result);
+        ScanActivity.this.finish();
+    }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if(keyCode==131){
@@ -98,5 +130,7 @@ public class ScanActivity extends AppCompatActivity implements OnClickListener {
     protected void onDestroy() {
         super.onDestroy();
         deviceManager.onDestory();
+        unregisterReceiver(receiver);
+        SystemProperties.set("persist.sys.scanstopimme", "true");
     }
 }
